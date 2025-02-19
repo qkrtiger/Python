f3ab238e83e789655ff32062989e8fee1235c5bb
PARK HO YEON <133315262+qkrtiger@users.noreply.github.com>
2025-02-19 14:47:19 +0900

    Create timer.py

---
+++ TIL/timer.py
@@ -0,0 +1,22 @@
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        
+        # 실행 시간 계산
+        elapsed_time = end_time - start_time
+        
+        # 시간, 분, 초로 변환
+        hours, remainder = divmod(elapsed_time, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        
+        print(f"{func.__name__} 실행 시간: {int(hours)}시간 {int(minutes)}분 {seconds:.2f}초")
+        return result
+    return wrapper
+
+# timer 함수 사용
+@timer
+def sample(self):
+  print('Hello')
+  time.sleep(5)
